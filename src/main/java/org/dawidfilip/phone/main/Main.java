dc5d34acddd6ffa585894951e89bca93a30d4cc1
dawid <dpilif@gmail.com>
2018-07-15 12:53:13 +0200

    Added missing resources and test/java directory.

--- src/main/java/org/dawidfilip/phone/main/Main.java
+++ src/main/java/org/dawidfilip/phone/main/Main.java
@@ -2,10 +2,23 @@ package org.dawidfilip.phone.main;
 
 import static org.dawidfilip.common.PrintBulider.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 
 	public static void main(String[] args) {
 		print("Starting application: '" + Main.class.getSimpleName() + "' class executing!");
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unitPhoneDB");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		
 	}	
 
 }
