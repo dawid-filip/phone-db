dc325a52858f66171b69020d9989b1b2439b421b
dawid <dpilif@gmail.com>
2018-07-17 22:28:41 +0200

    Added PhoneTest test web e2e service.

--- src/main/java/org/dawidfilip/webservice/PhoneWebWebService.java
+++ src/main/java/org/dawidfilip/webservice/PhoneWebWebService.java
@@ -1,7 +1,17 @@
 package org.dawidfilip.webservice;
 
+import java.math.BigDecimal;
+import java.util.Random;
 import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+
+import org.dawidfilip.dao.PhoneTestDao;
+import org.dawidfilip.phone.app.configuration.ApplicationBeanConfiguration;
+import org.dawidfilip.phone.common.PhoneTestBuilder;
+import org.dawidfilip.phone.model.PhoneTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +23,20 @@ public class PhoneWebWebService {
 	
 	private Logger logger = Logger.getLogger(PhoneWebWebService.class.getSimpleName());
 	
+	private static AbstractApplicationContext context = new AnnotationConfigApplicationContext(ApplicationBeanConfiguration.class); 
+	
+	@RequestMapping(path = "/add/brand/{brand}/model/{model}", method = RequestMethod.GET)
+	public String add(@PathVariable("brand") String brand,
+					  @PathVariable("model") String model) {
+		PhoneTest pt = new PhoneTest(brand, model + "-" + 
+					  new Random().nextInt(100) + 1, 
+					  new Random().nextDouble()+10000, new BigDecimal("100"));
+		
+		PhoneTestDao phoneTestDao = context.getBean("phoneTestDao", PhoneTestDao.class);
+		phoneTestDao.persist(pt);
+		
+		return pt.toString();
+	}
 	
 	@RequestMapping(path = "/bean", method = RequestMethod.GET)
 	public String getString() {
